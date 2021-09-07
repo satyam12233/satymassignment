bec690819fa0dccafd5c0eefc50b1e1aaacbd22e
satyam12233 <47660853+satyam12233@users.noreply.github.com>
2021-09-07 15:37:10 +0530

    Add files via upload

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,17 @@
+package assignment.demo;
+
+public class MultiplyTwoNumbers {
+	
+	public static void main(String arg[]) {
+		//variable declaration
+		
+	float numberone = (float) 12.3;
+	float numbertwo = (float) 3.5;
+	
+	// temp variable declaration
+	
+	float temp = numberone * numbertwo;
+	System.out.println("Multiplication Of Two Floating Point Numbers = " +temp);
+
+}
+}
